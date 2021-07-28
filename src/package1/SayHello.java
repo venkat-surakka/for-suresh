077815f83a9d3740d1147fb16dbc8f94e4977026
Venkat Surakka <venkat.surakka@outlook.com>
2021-07-28 21:07:37 +1200

    added Java project

---
+++ src/package1/SayHello.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Java program");
+
+	}
+
+}
