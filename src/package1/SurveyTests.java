f497ce6cca5614dc43dd23e5856c683ff04ac175
Venkat Surakka <venkat.surakka@outlook.com>
2021-07-29 15:55:01 +1200

    Updated tests with Wee Haus comments

--- src/package1/SurveyTests.java
+++ src/package1/SurveyTests.java
@@ -3,8 +3,7 @@ package package1;
 public class SurveyTests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Added survey tests");
+		System.out.println("Added survey regression tests");
 	}
 
 }
